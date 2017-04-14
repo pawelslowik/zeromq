ca36a7bf384942fc7856414549b067767df9ec41
psl <pawel.slowik.dev@gmail.com>
2017-04-14 20:14:16 +0200

    Added comments for PAIR implementation

--- jeromq/src/main/java/pl/com/psl/zeromq/jeromq/pair/PairZeroMQClient.java
+++ jeromq/src/main/java/pl/com/psl/zeromq/jeromq/pair/PairZeroMQClient.java
@@ -16,6 +16,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by psl on 14.04.17.
+ *
+ * PAIR pattern allows client to connect directly to server.
+ * After sending request, client doesn't have to wait for response,
+ * it can send consecutive requests and after that, get all responses.
+ *
+ * There can be only one connected peer.
  */
 @Component
 @Profile(Profiles.PAIR)
